3626090ad8076775e48fbd2cb7b6ce31e7707a76
Devin Guillory <deving2009@gmail.com>
2016-02-18 18:36:01 -0800

    null default

--- app/code/community/Blackbird/Merlinsearch/Adminhtml/Model/System/Config/Source/Product/Attributes.php
+++ app/code/community/Blackbird/Merlinsearch/Adminhtml/Model/System/Config/Source/Product/Attributes.php
@@ -24,7 +24,7 @@ class Blackbird_Merlinsearch_Adminhtml_Model_System_Config_Source_Product_Attrib
 		$options[] = $item;
 	}
     usort($options, compare_label);
-	$options[] = array('value' => null, 'label' => 'None');
+	$options[] = array('value' => null, 'label' => null);
     return $options;
 	}
 }		
